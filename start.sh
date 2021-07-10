91b52ead4d0121e1e5c6051178b6c24db8e0e448
Carlos Ponce <cponce@alumnos.inf.utfsm.cl>
2021-07-10 16:52:43 -0400

    Add quote to GRAPH_API_URL

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 set -xeuo pipefail
 
-sed -i "s|GRAPH_API_URL_PLACEHOLDER|$GRAPH_API_URL|g" -i public/scripts/controllers/edit.js
+sed -i "s|GRAPH_API_URL_PLACEHOLDER|'$GRAPH_API_URL'|g" -i public/scripts/controllers/edit.js
 node server.js
